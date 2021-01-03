806637481891c3b8296fa8e235465b40d5362406
amith jain <amith@apples-MacBook-Pro.local>
2021-01-03 16:35:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import SocialBuilder from './components/SocialBuilder';
+import { Route, Switch } from 'react-router';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route  path="/login" component={Login}></Route>
+        <Route path='/signup' component={SignUp}></Route>
+        <Route path='/' component={SocialBuilder}></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
